2e1535f7c9a32de4dc91b223e615ee66736095af
Dusty Phillips <dusty@archlinux.ca>
2011-12-19 17:33:27 +0100

    I am starting afresh. Google gears is dead and Todoy used to be Offline-focused. The new version will be online-offline, more HTML5ified, jquery-mobiled, and essentially awesome.

--- todos/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_index_redirect(self):
-        self.assertRedirects(self.client.get('/'), '/todos/', status_code=301)
--- todos/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render_to_response
-
-def day(request):
-    return render_to_response('day.html')
-
