6fffa6b3e69710eaff8099857d65659d45adcee7
Dusty Phillips <dusty@archlinux.ca>
2011-12-25 17:25:10 -0600

    Make the jquery-mobile local.

--- todoy/urls.py
+++ todoy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,3 +16,9 @@ urlpatterns = patterns('todoy.todos.views',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
