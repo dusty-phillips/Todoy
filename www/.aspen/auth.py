699914ea988bfd659d57bd5a7a30ce6baf358fe9
Dusty Phillips <dusty@buchuki.com>
2012-03-23 19:34:41 -0600

    Switch to aspen for serving

---
+++ www/.aspen/auth.py
@@ -0,0 +1,9 @@
+from aspen import Response
+
+
+def inbound(request):
+    print("inbound")
+
+
+def outbound(request):
+    print("outbound received")
---
+++ www/js/pyjados.js
@@ -0,0 +1,9 @@
+var setup = function() {
+    var __kwargs = __kwargs_get(arguments);
+    var __varargs = __varargs_get(arguments);
+    var $v1 = Array.prototype.slice.call(arguments).concat(js(__varargs));
+    __builtins__.PY$print(str('Pyjados Hello World'));
+return None;
+}
+jQuery(js(setup));
+
