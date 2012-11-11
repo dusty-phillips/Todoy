3aef2aba5d16c3fea2f9473be689fc0af106b9d6
Dusty Phillips <dusty@linux.ca>
2012-11-11 12:50:33 -0700

    Not much point in shipping as a package because Kivy breaks all the packaging standards.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name = "Todoy",
-    version = "0.1",
-    packages = find_packages(),
-    install_requires=[
-        'kivy>1.4.1'
-    ]
-)
