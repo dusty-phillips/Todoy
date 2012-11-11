ab6e9e077a02b11d6b1d14736d979f3fd565f93f
Dusty Phillips <dusty@linux.ca>
2012-11-11 12:50:53 -0700

    Basic Kivy App

---
+++ main.py
@@ -0,0 +1,11 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+if __name__ == '__main__':
+    MyApp().run()
