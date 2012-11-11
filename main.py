34571461bf014d5b666c2e466166b6f9075e2c18
Dusty Phillips <dusty@linux.ca>
2012-11-11 12:58:32 -0700

    Some hackish interface. I want to work on using gestures.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.button import Button
 
-class MyApp(App):
-    def build(self):
-        return Button(text='Hello World')
+class TodoyApp(App):
+    pass
 
 if __name__ == '__main__':
-    MyApp().run()
+    TodoyApp().run()
